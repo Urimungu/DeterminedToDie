0fb87bfc242658e566b724f16dd5b40b637e5b7a
Elijah <enutter24@gmail.com>
2020-06-16 17:56:55 -0700

    FixingMenuPrefabs

---
+++ Assets/Scripts/Menu/MenuData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuData : MonoBehaviour
+{
+    [Header("Menus")]
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject characterSelect;
+    public GameObject credits;
+    public GameObject victory;
+    public GameObject gameOver;
+    public GameObject escMenu;
+}
