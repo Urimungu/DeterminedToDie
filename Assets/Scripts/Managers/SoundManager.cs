80f659300f505f0dc8a18dbc9143295b685d1017
Elijah <enutter24@gmail.com>
2020-08-16 04:34:40 -0700

    SoundManager

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    [Header("Audio Sources")]
+    public AudioSource fx;
+    public AudioSource music;
+    public AudioSource dialogue;
+
+    [Header("Pitch")]
+    public float lowPitch = 0.95f;
+    public float highPitch = 1.05f;
+
+    public void PlaySound(AudioClip clip, AudioSource source)
+    {
+        if(source == fx)
+        {
+            float pitch = Random.Range(lowPitch, highPitch);
+            source.pitch = pitch;
+        }
+        source.clip = clip;
+        source.Play();
+    }
+}
